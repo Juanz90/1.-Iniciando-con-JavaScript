38d274241cd8f1bff86ef3eae7e29dc550689248
Juanz90 <157052984+Juanz90@users.noreply.github.com>
2024-01-18 14:51:57 -0300

    Add files via upload

---
+++ practica1.js
@@ -0,0 +1,20 @@
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
+
+let nombre = 'Luna';
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert('Error! Completa todos los campos');
+
+let mensajeDeError = '¡Error! Completa todos los campos';
+
+alert(mensajeDeError);
+
+let nombreUsuario = prompt('¿Cuál es su nombre?');
+let edadUsuario = prompt('¿Cuál es su edad?');
+
+if (edadUsuario >= 18) {
+
+    alert ('¡Puedes obtener tu licencia de conducir!');
+}
